a0e84d2f8123d9786e70164f4e58f951110c0dc9
Agustin <agustin.devfs@gmail.com>
2025-02-28 17:46:49 -0300

    data provider url railway

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Función para convertir datos en FormData
 const convertDataToFormData = (data) => {
